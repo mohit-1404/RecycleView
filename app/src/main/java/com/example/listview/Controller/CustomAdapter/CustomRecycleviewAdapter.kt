b3dad61d3e0559aedc91b021df49edd4c97583d8
mohit-1404 <108791847+mohit-1404@users.noreply.github.com>
2024-05-25 23:29:08 +0530

    update CustomRecycleviewAdapter

--- app/src/main/java/com/example/listview/Controller/CustomAdapter/CustomRecycleviewAdapter.kt
+++ app/src/main/java/com/example/listview/Controller/CustomAdapter/CustomRecycleviewAdapter.kt
@@ -10,33 +10,35 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.listview.Controller.Model.Category
 import com.example.listview.R
 
-    class CustomRecycleviewAdapter(private val context: Context, private val categories:List<Category> , val itemClick:(Category)->Unit) : RecyclerView.Adapter<CustomRecycleviewAdapter.Holder>() {
+class CustomRecycleviewAdapter(
+    private val context: Context,
+    private val categories: List<Category>,
+    private val itemClick: (Category) -> Unit
+) : RecyclerView.Adapter<CustomRecycleviewAdapter.Holder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): Holder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val view = LayoutInflater.from(context).inflate(R.layout.cusotmadapterview, parent, false)
-        return Holder(view,itemClick)
+        return Holder(view)
     }
 
-    inner class Holder(itemView: View,val itemClick:(Category)->Unit) : RecyclerView.ViewHolder(itemView) {
-        val categorieName = itemView.findViewById<TextView>(R.id.categoryName)
-        val categorieImage = itemView.findViewById<ImageView>(R.id.categoryImage)
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val categoryName: TextView = itemView.findViewById(R.id.categoryName)
+        private val categoryImage: ImageView = itemView.findViewById(R.id.categoryImage)
 
-        fun bind(context: Context, categories: List<Category>, position: Int) {
-            val category = categories[position]
+        fun bind(category: Category) {
             val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-            categorieImage.setImageResource(resourceId)
-            categorieName.text = category.name
-//            On Click Listener
+            categoryImage.setImageResource(resourceId)
+            categoryName.text = category.name
+
             itemView.setOnClickListener { itemClick(category) }
         }
     }
 
     override fun getItemCount(): Int {
-        return categories.count()
+        return categories.size
     }
 
-    override fun onBindViewHolder(holder: Holder, p1: Int) {
-        holder.bind(context,categories,p1)
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.bind(categories[position])
     }
 }
-
